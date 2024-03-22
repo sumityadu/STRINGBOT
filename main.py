12019254895aba91322b16f664087332baff44ca
SAGE_XD <159287148+Sagexdd@users.noreply.github.com>
2024-03-22 22:46:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = Client(
     api_hash=config.API_HASH,
     bot_token=config.BOT_TOKEN,
     in_memory=True,
-    plugins=dict(root="DAXXSTRINGBOT"),
+    plugins=dict(root="STRINGBOT"),
 )
 
 
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     except AccessTokenInvalid:
         raise Exception("Your BOT_TOKEN is not valid.")
     uname = app.get_me().username
-    print(f"@{uname} 𝖲𝖳𝖠𝖱𝖳𝖤𝖣 𝖲𝖴𝖢𝖤𝖲𝖲𝖥𝖴𝖫𝖫𝖸. 𝖬𝖠𝖣𝖤 𝖡𝖸 @ 𝖣𝖠𝖷𝖷 𝖳𝖤𝖠𝖬 🤗")
+    print(f"@{uname} 𝖲𝖳𝖠𝖱𝖳𝖤𝖣 𝖲𝖴𝖢𝖤𝖲𝖲𝖥𝖴𝖫𝖫𝖸. 𝖬𝖠𝖣𝖤 𝖡𝖸 SYNAXX 🤗")
     idle()
     app.stop()
     print("𝗕𝗢𝗧 𝗦𝗧𝗢𝗣𝗣𝗘𝗗!")
